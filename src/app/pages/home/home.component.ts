58d7b66a44f21f3c8b9d51e048e0a94e00cd0c18
GiselleFerrari <giselleaferrari@gmail.com>
2021-07-31 02:05:25 -0300

    Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,33 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  title = 'Moonlights';
+    subtitle = '¡Bienvenidos a Moonlights Bakery! Cada producto está realizado con mucho amor ♥';
+  
+    cards = [
+      {
+      imagen: "/assets/images/tarjetas/lemon.jpg",
+      categoria: "Un relleno suave de limón especiado, con merengue esponjoso y tostado.",
+      titulo: "Lemon Pie",
+      precio: 600,
+    },{
+      imagen: "/assets/images/tarjetas/chocotorta.jpg",
+      categoria: "Un clásico de chocolate, dulce de leche, queso crema y decoraciones a gusto.",
+      titulo: "Chocotorta",
+      precio: 500,
+    },{
+      imagen: "/assets/images/tarjetas/oreo1.jpg",
+      categoria: "Helado casero de oreo, con bizcocho de chocolate y crema de oreo.",
+      titulo: "Torta Oreo",
+      precio: 800,
+    },{
+      imagen: "/assets/images/tarjetas/cheesec.jpg",
+      categoria: "Tarta a base de queso crema con frutos naturales: Arándanos,  frambuesas, moras y frutillas.",
+      titulo: "Cheesecake de Frutos Rojos",
+      precio: 700,
+    }
+  ]
+
   ngOnInit(): void {
   }
 
