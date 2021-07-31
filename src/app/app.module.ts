3b5cc6212afecda6687702a70e73e3d85ddc8f42
GiselleFerrari <giselleaferrari@gmail.com>
2021-07-30 22:45:01 -0300

    Comienzo del Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SobremiComponent } from './pages/sobremi/sobremi.component';
+import { RecetasComponent } from './pages/recetas/recetas.component';
+import { TitleComponent } from './components/title/title.component';
+import { NavComponent } from './components/nav/nav.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobremiComponent,
+    RecetasComponent,
+    TitleComponent,
+    NavComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
