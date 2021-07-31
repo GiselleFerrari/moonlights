b684aaca991d82812476225a3a530754449d1680
GiselleFerrari <giselleaferrari@gmail.com>
2021-07-31 02:06:27 -0300

    Index + App comp/routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { RecetasComponent } from './pages/recetas/recetas.component';
+import { SobremiComponent } from './pages/sobremi/sobremi.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recetas', component: RecetasComponent },
+  { path: 'sobremi', component: SobremiComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
