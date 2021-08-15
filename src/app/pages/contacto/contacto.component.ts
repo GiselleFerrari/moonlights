feb4ef90fc0524b9456d5999111721448e4172f3
GiselleFerrari <giselleaferrari@gmail.com>
2021-08-15 20:50:29 -0300

    Contacto

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ContactoComponent implements OnInit {
 
   constructor() { }
+
+  @Input() contacto = "/assets/images/contacto/zorrito.png";
 
   ngOnInit(): void {
   }
